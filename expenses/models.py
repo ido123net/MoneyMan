5e5969aefeb71d7fcb5d91caf1c00b311883d7a9
Ido Frenkel <ido.frenkel@mobileye.com>
2024-05-09 15:17:53 +0300

    Add '__str__' to Expense class

--- expenses/models.py
+++ expenses/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
-# Create your models here.
 
 class Expense(models.Model):
     title = models.CharField(max_length=128)
     amount = models.DecimalField(decimal_places=2, max_digits=12)
     date = models.DateField()
+
+    def __str__(self) -> str:
+        return f'[#{self.id}] {self.title} @ {self.date} {self.amount}'
